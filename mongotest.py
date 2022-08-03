a7bad5ca33bc6f0c67f0ee956bb8775b7cabc6f3
sambit paul <paulsambit2@gmail.com>
2022-08-03 19:26:57 +0530

    yes i am commiting bro

---
+++ mongotest.py
@@ -0,0 +1,15 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://sambit035:sambit03513@sambit035.zdgvx.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d = {
+    "name" : "Sambit",
+    "email " : "paulsambit2@gmail.com",
+    "surname" : "paul"
+}
+
+db1 = client['mangotest']
+coll = db1['test']
+coll.insert_one(d)
